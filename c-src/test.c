e27ceb5bca4d2a0ed184afd588344316451d89e4
karintomania <19652340+karintomania@users.noreply.github.com>
2025-07-31 12:39:17 +0100

    add test.c

--- c-src/test.c
+++ c-src/test.c
@@ -3,14 +3,30 @@
 #define TB_OPT_ATTR_W 64
 #define TB_OPT_EGC
 #include "termbox2.h"
+#include "locale.h"
 
 int main() {
+    setlocale(LC_ALL, "");
+
     tb_init();
 
-    tb_print(0,0,TB_DEFAULT, TB_DEFAULT, "aa");
+    size_t w;
+
+    tb_print(0,0,TB_DEFAULT, TB_DEFAULT, "emoji 🧑‍💻");
     tb_present();
 
     struct tb_event ev;
+
+    tb_poll_event(&ev);
+
+    tb_clear();
+
+    // if I add those lines, the emoji works.
+    // tb_print(0,0,TB_DEFAULT, TB_DEFAULT, "          ");
+    // tb_present();
+
+    tb_print(0,0,TB_DEFAULT, TB_DEFAULT, "emoji 👩‍💻");
+    tb_present();
 
     tb_poll_event(&ev);
 
